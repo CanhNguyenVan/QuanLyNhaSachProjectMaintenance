37261318e9151950b87b44e721d71d40e7a23134
CanhNguyenVan <12520034@gm.uit.edu.vn>
2015-07-02 18:08:31 +0700

    Thông tin

---
+++ test/frmThongTin.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace test
+{
+    partial class frmThongTin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VẬN HÀNH PHÁT TRIỂN BẢO TRÌ PHẦN MỀM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhóm N06 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(198, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Đồ Án Quản Lý Nhà Sách";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(326, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Giáo viên hướng dẫn : ThS. Nguyễn Công Hoan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(191, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "12520026 - Phan Y Biển";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(191, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "12520034 - Nguyễn Văn Cảnh";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(191, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(259, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "12520042 - Nguyễn Sỹ Mạnh Cường";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(191, 236);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(204, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "12520360 - Phạm Hồng Sơn";
+            // 
+            // frmThongTin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(612, 369);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmThongTin";
+            this.Text = "Thông Tin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
